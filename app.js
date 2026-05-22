f4a7e549f24ff070c8392ecf4663f19c4b7c89ec
Boluwatife Muhammed <boluwatifemuhammed8@gmail.com>
2026-05-22 19:54:01 +0100

    updated

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
-const bookmarkForm  = document.getElementById("bookmark-form")
-const textInput  = document.getElementById("text-input")
-const linkInput  = document.getElementById("link-input") 
-const bookmarkList  = document.getElementById("bookmark-list") 
+const bookmarkForm = document.getElementById("bookmark-form")
+const linkInput = document.getElementById("link-input")
+const textInput = document.getElementById("text-input")
+const bookmarkList = document.getElementById("bookmark-list")
 
+bookmarkForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const linkValue = linkInput.value;     
+    const textValue = textInput.value;
+    
+    const bookmark = document.createElement("div");
+    bookmark.classList.add(".bookmark")
+    bookmark.innerHTML = `
+        <span>${textValue}</span>
+        <a href = "${linkValue}"> <button>Open Link</button> </a>
+    `
+    bookmarkList.appendChild(bookmark);
+    bookmarkForm.reset();
+})
 
-bookmarkForm.addEventListener("submit", function(e){
+bookmarkForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const textValue = textInput.value;
     const linkValue = linkInput.value;
+    const textValue = textInput.value;
 
-    console.log(textValue, linkValue);
-
-    const bookmark = document.createElement("div");
-    bookmark.classList.add("bookmark-item")
-
+    const bookmark = document.createElement("div")
+    bookmark.classList.add(".bookmark")
     bookmark.innerHTML = `
-        <span class = "text-value">${textValue}</span>
-            <a href="${linkValue}">
-             <button class = "link-value">Open Link</button>
-            </a>
+        <p>${textValue}</p>
+        <h1>${linkValue}</h1>
+
     `
 
     bookmarkList.appendChild(bookmark);
-
-
-    bookmarkForm.reset();
-
+    bookmarkForm.reset ();
 })
+
 
